1f4ec23b955e21b40934440a504888e6e4373381
Anshul Kapoor <anshulkapoor018@gmail.com>
2017-11-23 22:39:15 +0530

    Dynamic data adding

--- src/app.js
+++ src/app.js
@@ -7,45 +7,45 @@ const app = {
 };
 
 const onFormSubmit = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const option = e.target.elements.option.value
-    
-    if(option){
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        render();
-    }
-};
+  const option = e.target.elements.option.value;
 
-const removeAll = () => {
-    app.options = [];
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
     render();
+  }
 };
- 
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
-    const template = (
-        <div>
-          <h1>{app.title}</h1>
-          {app.subtitle && <p>{app.subtitle}</p>}
-          <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-          <p>{app.options.length}</p>
-          <button onClick = {removeAll}>Remove All</button>
-          <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-          </ol>
-      
-          <form onSubmit={onFormSubmit}>
-              <input type = "text" name = "option"/>
-              <button>Add option</button>
-          </form>
-        </div>
-      );
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
 
-      ReactDOM.render(template, appRoot)
+  ReactDOM.render(template, appRoot);
 };
 
 render();
